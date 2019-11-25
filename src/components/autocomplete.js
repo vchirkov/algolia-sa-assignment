720d8c5aa831abfe7db3f38a2cb9843b9f495d70
Alex Webb <alex.webb@algolia.com>
2019-11-25 16:54:29 +0000

    Prepare

--- src/components/autocomplete.js
+++ src/components/autocomplete.js
@@ -11,10 +11,7 @@ class Autocomplete {
   }
 
   _registerClient() {
-    this._searchClient = algoliasearch(
-      'latency',
-      '6be0576ff61c053d5f9a3225e2a90f76'
-    );
+    this._searchClient = algoliasearch('', '');
 
     this._search = instantsearch({
       indexName: 'instant_search',
--- src/components/results-page.js
+++ src/components/results-page.js
@@ -12,10 +12,7 @@ class ResultPage {
   }
 
   _registerClient() {
-    this._searchClient = algoliasearch(
-      'latency',
-      '6be0576ff61c053d5f9a3225e2a90f76'
-    );
+    this._searchClient = algoliasearch('', '');
 
     this._search = instantsearch({
       indexName: 'instant_search',
