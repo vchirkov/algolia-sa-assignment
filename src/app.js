dbcfcd1a6661401dc6fc4c39ba7dace6e9f04783
Alex Webb <alex.webb@algolia.com>
2019-11-25 11:27:03 +0000

    es6ify

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-/* global algoliasearch instantsearch */
+import ResultsPage from './results-page';
 
-const searchClient = algoliasearch(
-  'latency',
-  '6be0576ff61c053d5f9a3225e2a90f76'
-);
-
-const search = instantsearch({
-  indexName: 'instant_search',
-  searchClient,
-});
-
-search.addWidgets([]);
-
-search.start();
+const resultPage = new ResultsPage();
