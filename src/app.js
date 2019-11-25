121587ae6868e615021dc447ac95aac3b75b6ad2
Alex Webb <alex.webb@algolia.com>
2019-11-25 13:29:38 +0000

    header styling, click functionality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 import ResultsPage from './Components/results-page';
 import Autocomplete from './Components/autocomplete';
 
-const autocomplete = new Autocomplete();
-const resultPage = new ResultsPage();
+class SpencerAndWilliamsSearch {
+  constructor() {
+    this._initSearch();
+    this._registerEvents();
+  }
+
+  _initSearch() {
+    this.autocompleteDropdown = new Autocomplete();
+    this.resultPage = new ResultsPage();
+  }
+
+  _registerEvents() {
+    const autocomplete = document.querySelector('.autocomplete');
+    const searchbox = document.querySelector('#searchbox input');
+
+    searchbox.addEventListener('click', () => {
+      autocomplete.style.display = 'block';
+    });
+
+    searchbox.addEventListener('blur', () => {
+      autocomplete.style.display = 'none';
+    });
+  }
+}
+
+new SpencerAndWilliamsSearch();
---
+++ src/components/autocomplete.js
@@ -0,0 +1,43 @@
+import algoliasearch from 'algoliasearch';
+import instantsearch from 'instantsearch.js';
+import { hits, searchBox, configure } from 'instantsearch.js/es/widgets';
+
+class Autocomplete {
+  constructor() {
+    this._registerClient();
+    this._registerWidgets();
+    this._startSearch();
+  }
+
+  _registerClient() {
+    this._searchClient = algoliasearch(
+      'latency',
+      '6be0576ff61c053d5f9a3225e2a90f76'
+    );
+
+    this._search = instantsearch({
+      indexName: 'instant_search',
+      searchClient: this._searchClient,
+    });
+  }
+
+  _registerWidgets() {
+    this._search.addWidgets([
+      configure({
+        hitsPerPage: 3,
+      }),
+      searchBox({
+        container: '#searchbox',
+      }),
+      hits({
+        container: '#autocomplete-hits',
+      }),
+    ]);
+  }
+
+  _startSearch() {
+    this._search.start();
+  }
+}
+
+export default Autocomplete;
